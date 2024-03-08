d717059e578ffaf60f7f1de2f9e9945c7736c92e
Smitgoti30 <smitgoti2103@gmail.com>
2024-03-08 10:49:09 +0530

    add Menu-Item to admin and customer - SG

--- Solution/frontend/src/components/Roms.js
+++ Solution/frontend/src/components/Roms.js
@@ -1,91 +1,32 @@
-import React, { useState } from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import Home from "./Home";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import HomeC from "./customer/Home";
 import NoPage from "./NoPage";
 import Customer from "./Customer";
-import MenuAdmin from "./MenuAdmin";
+import MenuAdmin from "./admin/menu/MenuAdmin";
 import AboutUs from "./AboutUs";
 import Contact from "./Contact";
+import Layout from "./theme/layout";
 import CategoryLayout from "./CategoryLayout";
-import Login from "./Login";
 
-function Roms(props) {
+function Roms() {
   return (
     <>
-      <main>
-        <Routes>
-          <Route path="/" element={<Header />}>
-            <Route index element={<><br></br><CategoryLayout /></>} />
-            <Route
-              path="menu"
-              element={
-                <div className="p-4">
-                  {/* <Menu Search />
-                    <h2>Menu List</h2>
-                    <MenuTable /> */}
-                </div>
-              }
-            />
-            <Route
-              path="/customer"
-              element={
-                <>
-                  <br></br>
-                  <Customer />
-                </>
-              }
-            />
-            <Route
-              path="/menu_customer"
-              element={
-                <>
-                  <br></br>
-                  <CategoryLayout />
-                </>
-              }
-            />
-            <Route
-              path="/menu_admin"
-              element={
-                <>
-                  <br></br>
-                  <MenuAdmin />
-                </>
-              }
-            />
-            <Route
-              path="/about"
-              element={
-                <>
-                  <br></br>
-                  <AboutUs />
-                </>
-              }
-            />
-            <Route
-              path="/contact"
-              element={
-                <>
-                  <br></br>
-                  <Contact />
-                </>
-              }
-            />
-            <Route
-              path="/login"
-              element={
-                <>
-                  <Login />
-                </>
-              }
-            />
+      <Layout>
+        <main>
+          <Routes>
+            <Route index element={<HomeC />} />
+            <Route path="/customer" element={<Customer />} />
+            <Route path="/customer/menu" element={<CategoryLayout />} />
+            <Route path="/admin/menu" element={<MenuAdmin />} />
+            <Route path="/admin/home" element={<Home />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/contact" element={<Contact />} />
             <Route path="*" element={<NoPage />} />
-          </Route>
-        </Routes>
-      </main>
-      <Footer />
+          </Routes>
+        </main>
+      </Layout>
     </>
   );
 }
