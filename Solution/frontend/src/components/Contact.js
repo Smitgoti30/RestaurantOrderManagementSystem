09725f3cc2ac01cce47b5becdabb8873278322bb
Shivang Patel <shivangpatel0111@gmail.com>
2024-03-28 14:02:24 -0400

    Backend integration for Contact

--- Solution/frontend/src/components/Contact.js
+++ Solution/frontend/src/components/Contact.js
@@ -1,4 +1,25 @@
 import React, { useState } from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+const form = useRef();
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  emailjs
+    .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", form.current, {
+      publicKey: "YOUR_PUBLIC_KEY",
+    })
+    .then(
+      () => {
+        console.log("SUCCESS!");
+      },
+      (error) => {
+        console.log("FAILED...", error.text);
+      }
+    );
+};
 
 function Contact() {
   const [formData, setFormData] = useState({
@@ -20,48 +41,49 @@ function Contact() {
   return (
     <div className="row">
       <h1>Contact Us</h1>
-      <p className="text-center">Get in touch with us! We'd love to hear from you.</p>
+      <p className="text-center">
+        Get in touch with us! We'd love to hear from you.
+      </p>
       <div className="col-4"></div>
-      <form className="col-4 m-3 p-4 contact-us text-center" onSubmit={handleSubmit}>
+      <form
+        className="col-4 m-3 p-4 contact-us text-center"
+        onSubmit={sendEmail}
+      >
         <div>
-          <label>
-            Your Name*:
-          </label>
+          <label>Your Name*:</label>
           <input
             type="text"
             name="name"
             value={formData.name}
-            onChange={handleChange}
+            onChange={sendEmail}
             required
           />
         </div>
         <br />
         <div>
-          <label>
-            Your Email Address*:
-          </label>
+          <label>Your Email Address*:</label>
           <input
             type="email"
             name="email"
             value={formData.email}
-            onChange={handleChange}
+            onChange={sendEmail}
             required
           />
         </div>
         <br />
         <div>
-          <label>
-            Your Message*:
-          </label>
+          <label>Your Message*:</label>
           <textarea
             name="message"
             value={formData.message}
-            onChange={handleChange}
+            onChange={sendEmail}
             required
           ></textarea>
         </div>
         <br />
-        <button className="btn btn-red" type="submit">Send</button>
+        <button className="btn btn-red" type="submit" value="Send">
+          Send
+        </button>
       </form>
     </div>
   );
